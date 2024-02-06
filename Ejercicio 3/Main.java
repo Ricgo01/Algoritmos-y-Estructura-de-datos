0a069b7624e4c19210f5593a56dbc61dc6713636
Vann06 <127684976+Vann06@users.noreply.github.com>
2024-02-06 17:55:15 -0600

    Update Main.java

--- Ejercicio 3/Main.java
+++ Ejercicio 3/Main.java
@@ -1,3 +1,18 @@
+/*
+ * Algoritmos y Estructura de Datos
+ * 3 Semestre
+ * Facultad de Ingenieria
+ * Departamento de Ciencias de la Computacion
+ * Fecha inicio: 28/01/2024
+ * Fecha final: 06/02/2024
+ *
+ * @author Vianka Castro 23201
+ * 		   Ricardo Godinez 23247
+ * 
+ *  
+ * Clase Main 
+ *  
+*/
 
 
 
